20cd1457a5751314e181735fb9d496ae606f6688
Matteo Romani <kimosabe.mr@gmail.com>
2024-05-26 20:55:21 +0200

    crazy move for this guy

--- snake.c
+++ snake.c
@@ -4,54 +4,61 @@
 
 const int WIDTH = 50;
 const int HEIGHT = 20;
-struct Position {
-    int x;
-    int y;
-};
+int game_over = 1;
 enum Direction {
     UP,
     DOWN,
     LEFT,
     RIGHT 
 };
-struct Position snake_head_position = {25, 10}; 
+struct Position {
+    int x;
+    int y;
+};
+enum Direction current_direction = UP;
+struct Position snake_head_position = {25, 10};
 void detect_move();
 void draw_full_row();
 void draw_first_last_item_row(int h);
 void build();
+void update_head_position();
 
 
 int main(){
-    build();
+    while(game_over){
+        build();
+    }
     return 0;
 };
 
 
 void detect_move(){
-    char key = getch();
-    switch(key){
-        case 'w':
-            snake_head_position.y --;
-            build();
-            break;
-        case 's':
-            snake_head_position.y ++;
-            build();
-            break;
-        case 'a':
-            snake_head_position.x --;
-            build();
-            break;
-        case 'd':
-            snake_head_position.x ++;
-            build();
-            break;
-        default:
-            break;
+    if(kbhit()){
+        char key = getch();
+        switch(key){
+            case 'w':
+                current_direction = UP;
+                break;
+            case 's':
+                current_direction = DOWN;
+                break;
+            case 'a':
+                current_direction = RIGHT;
+                break;
+            case 'd':
+                current_direction = LEFT;
+                break;
+            case 'q':
+                game_over = 0;
+            default:
+                break;
+        }
     }
+    
 }
 
 void build(){
+    update_head_position();
     system("cls");
     void draw_full_row(){
     for (int i = 0; i <= WIDTH; i++){
@@ -78,7 +85,7 @@ void build(){
         } 
     };
 
-    //build environment
+    //build row by height
     for(int h = 0; h <= HEIGHT; h++){
         if(h == 0 || h == HEIGHT){
             draw_full_row();
@@ -86,5 +93,31 @@ void build(){
              draw_first_last_item_row(h);
         }
     }
+
+    printf("\n \n \n position x: %d", snake_head_position.x);
+    printf("\n position y: %d", snake_head_position.y);
+    printf("\n DIRECTION: %d", current_direction);
+
     detect_move();
+
 };
+
+
+void update_head_position(){
+    switch(current_direction){
+        case UP:
+            snake_head_position.y --;
+            break;
+        case DOWN:
+            snake_head_position.y ++;
+            break;
+        case RIGHT:
+            snake_head_position.x --;
+            break;
+        case LEFT:
+            snake_head_position.x ++;
+            break;
+        default:
+        break;
+    }
+}
